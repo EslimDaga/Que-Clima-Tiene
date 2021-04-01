8ea0f1848f9ee675e0cb6a0302b13a921b72f6ce
Eslim Daga <esliim9811@gmail.com>
2021-04-01 17:04:25 -0500

    Add weather and error components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,2 +1,4 @@
 export { default as Result } from "./Result";
 export { default as Form } from "./Form";
+export { default as Weather } from "./Weather";
+export { default as Error } from "./Error";
