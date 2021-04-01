1062f45e0267841ca0d3d798eadf965eb0d14b64
Eslim Daga <esliim9811@gmail.com>
2021-04-01 12:25:59 -0500

    Add content

---
+++ src/components/Home/Result.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Result = () => {
+  return (
+    <div className="col-lg-6">
+      <div className="home-contact mt-4">
+        <p className="text-primary font-weight-bold">Time date and problem monitoring</p>
+        <h1 className="home-title mt-3">Super Fast Professional Invoicing For Freelancers</h1>
+        <p className="text-muted mt-4">
+          Quisque a diam mattis fermentum lectus tempuada <br />
+                  velit cras magna dictum porta dolor dapibus dolor Etiam congartue hendrerit fusce eget ligula.
+                </p>
+        <div className="mt-4 pt-3">
+          <a href="#!" className="btn btn-primary mr-3">Estimate project</a>
+          <a href="#!" className="btn btn-outline-primary">Our portfolio</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Result;
